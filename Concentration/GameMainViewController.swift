c4a0f8ba1f7f0ed2e5d237cda6d47d4b46bbf0d7
Matt Ao <mattyayoh@gmail.com>
2018-12-16 23:40:09 -0800

    Boilerplate Xcode project files, plus ViewControllers that we will need. Also filled out the HomeViewController

---
+++ Concentration/GameMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameMainViewController.swift
+//  Concentration
+//
+//  Created by Matthew Ao on 12/16/18.
+//  Copyright © 2018 AYOH. All rights reserved.
+//
+
+import UIKit
+
+class GameMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Concentration/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Concentration
+//
+//  Created by Matthew Ao on 12/16/18.
+//  Copyright © 2018 AYOH. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+   @IBOutlet var buttonRoundedBackgroundView: UIView!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      buttonRoundedBackgroundView.layer.cornerRadius = 25.0
+      buttonRoundedBackgroundView.backgroundColor = UIColor.init(displayP3Red: 0.95, green: 0.95, blue: 0.95, alpha: 0.9)
+   }
+
+   
+}
+
