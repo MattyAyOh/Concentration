d19d27e03a99d704c6974b11fafb16b9be8c5175
Matt Ao <mattyayoh@gmail.com>
2018-12-19 02:40:37 -0800

    Average time per pair

--- Concentration/History/HistoryViewController.swift
+++ Concentration/History/HistoryViewController.swift
@@ -23,7 +23,7 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
    
    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       let history = historyList[indexPath.row]
-      let header = "Matched \(history.totalPairs) pairs, in \(history.totalTime.inMinutes()):\(history.totalTime.inSeconds()), with \(history.movesMade) moves"
+      let header = "\(history.totalPairs) pairs, in \(history.totalTime.inMinutes()):\(history.totalTime.inSeconds()), with \(history.movesMade) moves"
       let subtitle = "\(history.dateCompleted)"
       
       let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell") ?? UITableViewCell.init(style: .subtitle, reuseIdentifier: "HistoryCell")
@@ -43,12 +43,12 @@ class HistoryViewController: UIViewController, UITableViewDataSource {
          totalPairs += $0.totalPairs
       }
       let averageNumPairs:Int = historyList.count > 0 ? totalPairs/historyList.count : 0
-      let averageTimePerPair:Int = totalTime > 0 ? totalPairs/totalTime : 0
+      let averageTimePerPair:Int = totalTime > 0 ? totalTime/totalPairs : 0
       
       totalTimeLabel.text = "\(totalTime.inMinutes()):\(totalTime.inSeconds())"
       totalPairsLabel.text = String(totalPairs)
       averageNumPairsLabel.text = String(averageNumPairs)
-      averageTimePerPairLabel.text = String(averageTimePerPair)
+      averageTimePerPairLabel.text = "\(averageTimePerPair.inMinutes()):\(averageTimePerPair.inSeconds())"
       
       historyTable.reloadData()
    }
