1760ca4ff8a8d01ff64eb261595ed963ce6ca783
Matt Ao <mattyayoh@gmail.com>
2018-12-19 01:54:18 -0800

    because there is no longer any special logic in the encoding/decoding, we can trust UIKit to provide the default Codable functionality

--- Concentration/ConcentrationModel.swift
+++ Concentration/ConcentrationModel.swift
@@ -34,30 +34,4 @@ struct ConcentrationModel:Codable {
    func numPairs() -> Int {
       return tilePlacementInGrid.count/2
    }
-   
-   //MARK: - Codable
-   
-   enum CodingKeys: String, CodingKey {
-      case completedTilePairs
-      case tilePlacementInGrid
-      case secondsPassed
-      case movesMade
-   }
-   
-   func encode(to encoder: Encoder) throws {
-      var container = encoder.container(keyedBy: CodingKeys.self)
-      try container.encode(completedTilePairs, forKey: .completedTilePairs)
-      try container.encode(tilePlacementInGrid, forKey: .tilePlacementInGrid)
-      try container.encode(secondsPassed, forKey: .secondsPassed)
-      try container.encode(movesMade, forKey: .movesMade)
-   }
-   
-   public init(from decoder: Decoder) throws {
-      let container = try decoder.container(keyedBy: CodingKeys.self)
-      self.completedTilePairs = try container.decode(Set<Int>.self, forKey: .completedTilePairs)
-      self.tilePlacementInGrid = try container.decode([Int].self, forKey: .tilePlacementInGrid)
-      self.secondsPassed = try container.decode(Int.self, forKey: .secondsPassed)
-      self.movesMade = try container.decode(Int.self, forKey: .movesMade)
-   }
-   
 }
