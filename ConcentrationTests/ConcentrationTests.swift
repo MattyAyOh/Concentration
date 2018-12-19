e2d5dda192d966a90fda039f096d51ad880b5389
Matt Ao <mattyayoh@gmail.com>
2018-12-19 14:25:00 -0800

    Tests!  Testing the grid setup, and the playthrough

--- ConcentrationTests/ConcentrationTests.swift
+++ ConcentrationTests/ConcentrationTests.swift
@@ -9,26 +9,65 @@
 import XCTest
 @testable import Concentration
 
-class ConcentrationTests: XCTestCase {
+class ConcentrationTests: XCTestCase, GridViewDelegate {
+   
+   static let numPairs = 25
+   var gridView:ConcentrationGridView? = nil
+   var backingModel:GameModel? = nil
+   
+   override func setUp() {
+      let model = GameModel(numPairs: ConcentrationTests.numPairs)
+      backingModel = model
+      gridView = ConcentrationGridView.init(frame: CGRect(x: 0, y: 0, width: 500, height: 500))
+      gridView?.loadFromModel(model)
+      gridView?.delegate = self
+   }
+   
+   override func tearDown() {
+      // Put teardown code here. This method is called after the invocation of each test method in the class.
+   }
+   
+   func testTileCount() {
+      XCTAssert(gridView?.tiles.count == 50)
+   }
+   
+   func testPlaythrough() {
+      guard let numPairs = backingModel?.numPairs() else {XCTAssert(false); return}
+      guard let numTiles = gridView?.tiles.count else {XCTAssert(false); return}
+      for _ in 0..<numPairs {
+         var firstTile: ConcentrationTile? = nil
+         for j in 0..<numTiles {
+            guard let tile = gridView?.tiles[j] else {continue}
+            guard backingModel?.completedTilePairs.contains(tile.pairNumber) == false else {continue}
+            if firstTile == nil {
+               firstTile = tile
+               continue
+            }
+            
+            firstTile?.tileTapped(UITapGestureRecognizer())
+            tile.tileTapped(UITapGestureRecognizer())
+            if gridView?.lockFlipping == true {
+               tile.tileTapped(UITapGestureRecognizer())
+            }
+         }
+      }
+      
+      XCTAssert(backingModel?.completedTilePairs.count == ConcentrationTests.numPairs)
+   }
+   
+   func pairGuessed() {
+//      backingModel?.movesMade += 1
+   }
+   
+   func pairFound(_ pairNum: Int) {
+      backingModel?.completedTilePairs.insert(pairNum)
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+      // NOTE: This would be useful for testing the history persistance
+//
+//      if backingModel?.completedTilePairs.count == backingModel?.numPairs() {
+//         let win = HistoryModel(dateCompleted: Date(), timePassed: (self.backingModel?.secondsPassed ?? 0), movesMade: (self.backingModel?.movesMade ?? 0), totalPairs:(self.backingModel?.numPairs() ?? 0))
+//         self.backingModel = nil
+//      }
+   }
+   
 }
