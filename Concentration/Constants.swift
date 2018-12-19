7b325e062d4b478d84bb0acffe720d51ab4623fc
Matt Ao <mattyayoh@gmail.com>
2018-12-19 13:33:05 -0800

    add more images, up to 25 (hard difficulty)

--- Concentration/Constants.swift
+++ Concentration/Constants.swift
@@ -11,6 +11,30 @@ import UIKit
 struct K {
    static let defaultNumPairs = 10
    static let pairNumToImage: [Int:UIImage?] = [
-      0:UIImage(imageLiteralResourceName: "Apple")
+      0:UIImage(imageLiteralResourceName: "Aim"),
+      1:UIImage(imageLiteralResourceName: "AirBalloon"),
+      2:UIImage(imageLiteralResourceName: "Airplane"),
+      3:UIImage(imageLiteralResourceName: "Alarm"),
+      4:UIImage(imageLiteralResourceName: "Alert"),
+      5:UIImage(imageLiteralResourceName: "Anchor"),
+      6:UIImage(imageLiteralResourceName: "Apple"),
+      7:UIImage(imageLiteralResourceName: "Atomic"),
+      8:UIImage(imageLiteralResourceName: "AvatarMan"),
+      9:UIImage(imageLiteralResourceName: "AvatarNeutral"),
+      10:UIImage(imageLiteralResourceName: "AvatarWoman"),
+      11:UIImage(imageLiteralResourceName: "BabyBottle"),
+      12:UIImage(imageLiteralResourceName: "Barrel"),
+      13:UIImage(imageLiteralResourceName: "Baseball"),
+      14:UIImage(imageLiteralResourceName: "Basketball"),
+      15:UIImage(imageLiteralResourceName: "Bath"),
+      16:UIImage(imageLiteralResourceName: "Battery"),
+      17:UIImage(imageLiteralResourceName: "BatteryCharging"),
+      18:UIImage(imageLiteralResourceName: "Bean"),
+      19:UIImage(imageLiteralResourceName: "Bed"),
+      20:UIImage(imageLiteralResourceName: "Beer"),
+      21:UIImage(imageLiteralResourceName: "Bell"),
+      22:UIImage(imageLiteralResourceName: "Bicycle"),
+      23:UIImage(imageLiteralResourceName: "Binoculars"),
+      24:UIImage(imageLiteralResourceName: "BioHazard")
    ]
 }
