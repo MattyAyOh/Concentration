b35c60464d1d35f22e2826b5fd0f276084c66c59
Matt Ao <mattyayoh@gmail.com>
2018-12-22 18:53:13 -0500

    improve UX for game selection screen

--- Concentration/View Controllers/GameSelectViewController.swift
+++ Concentration/View Controllers/GameSelectViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class GameSelectViewController: UIViewController, UITextFieldDelegate {
    
+   @IBOutlet var numPairsLabel: UILabel!
    @IBOutlet var numPairsTextField: UITextField!
+   @IBOutlet var easyButton: UIButton!
+   @IBOutlet var mediumButton: UIButton!
+   @IBOutlet var hardButton: UIButton!
+   @IBOutlet var customButton: UIButton!
+   @IBOutlet var startButton: UIButton!
+   
+   lazy var allDifficultyButtons = [easyButton, mediumButton, hardButton, customButton]
    
    override func viewDidLoad() {
       super.viewDidLoad()
       numPairsTextField.text = String(K.defaultNumPairs)
       numPairsTextField.delegate = self
+      
+      startButton.isHidden = true
+      numPairsLabel.isHidden = true
+      numPairsTextField.isHidden = true
+   }
+   
+   func difficultySelected(_ button:UIButton) {
+      for btn in allDifficultyButtons {
+         btn?.isSelected = false
+      }
+      button.isSelected = true
+
+      numPairsLabel.isHidden = (button != customButton)
+      numPairsTextField.isHidden = (button != customButton)
+      
+      startButton.isHidden = false
    }
    
    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -29,15 +53,21 @@ class GameSelectViewController: UIViewController, UITextFieldDelegate {
    
    @IBAction func easyPressed(_ sender: Any) {
       numPairsTextField.text = "5"
+      difficultySelected(easyButton)
    }
    
    @IBAction func mediumPressed(_ sender: Any) {
       numPairsTextField.text = "10"
-      
+      difficultySelected(mediumButton)
    }
    
    @IBAction func hardPressed(_ sender: Any) {
       numPairsTextField.text = "25"
+      difficultySelected(hardButton)
+   }
+   
+   @IBAction func customPressed(_ sender: Any) {
+      difficultySelected(customButton)
    }
    
    @IBAction func startGamePressed(_ sender: Any) {
